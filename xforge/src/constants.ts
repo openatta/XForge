de0bc4a40227a812a1f6e2d0a1b1c6c6d2c8a5c6
xbitsmaster <45192161+xbitsmaster@users.noreply.github.com>
2026-08-11 01:41:11 -0700

    chore: release XForge v0.7.5

--- xforge/src/constants.ts
+++ xforge/src/constants.ts
@@ -1,5 +1,5 @@
 export const CLI_NAME = '@xforge/cli';
-export const CLI_VERSION = '0.7.4';
+export const CLI_VERSION = '0.7.5';
 export const PROTOCOL_VERSION = '2';
 export const API_VERSION = 'xforge.dev/v1alpha2';
 
