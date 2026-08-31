b1b902e0a6b5ee08ee309a84f131516032a86e25
xbitsmaster <45192161+xbitsmaster@users.noreply.github.com>
2026-08-31 06:24:25 -0700

    chore: prepare 0.8.0

--- xforge/src/constants.ts
+++ xforge/src/constants.ts
@@ -1,5 +1,5 @@
 export const CLI_NAME = '@xforge/cli';
-export const CLI_VERSION = '0.7.21';
+export const CLI_VERSION = '0.8.0';
 export const PROTOCOL_VERSION = '2';
 export const TARGETS = [
   'claude',
